f1fcae73cfc9f24d0ad0fcf3529daac08cfc5523
vilius <viliusj.jankauskas@gmail.com>
2018-06-12 19:53:34 +0300

    updated comments

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const socketIO = require('socket.io');
 const path = require('path');
 var beaconData = null;
 
+//Creating constants for port and index page that is launched when the server starts
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
+//The following code creates a server that is lisening for connections on port 3000
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
@@ -17,11 +19,13 @@ const io = socketIO(server);
 //Method that reacts to connctions to server
 io.on('connection', (socket) => {
   console.log('Client connected');
+
   //when a message is recived it's data is to beaconData variable
   socket.on('message', function(data){
     beaconData = data ;
   });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
 // in set time interval every client that is connected to server recieves message with beaconData value
 setInterval(() => io.emit('beaconData', beaconData), 500);
